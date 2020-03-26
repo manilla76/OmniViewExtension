583e328386b0c12acdbffa8170af946c943fa511
John Grothe <john.grothe@thermofisher.com>
2020-03-26 11:19:52 -0600

    Add project files.

---
+++ OmniViewExtension/OmniViewExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace OmniViewExtension
+{
+    public class OmniViewExtension : INotifyPropertyChanged
+    {
+        private int myVar;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int MyProperty
+        {
+            get { return myVar; }
+            set
+            {
+                if (myVar != value)
+                {
+                    myVar = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+    }
+}
